862fd0068c3eea1e8d71d37d90001387f1185e56
Carlos Correa <carlos.correa@mercadopago.com.br>
2011-03-30 14:51:09 -0300

    atualizacao da classe MercadoPago e Manual

---
+++ src/classes/MercadoPago.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ *  MercadoPago - PHP
+ *  Copyright (C) 2011  MercadoPago Brasil
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Verifica se os módulos CURL e JSON estão configurados no PHP
+ */
+if (!function_exists('curl_init')){
+    die ('MercadoPago needs the CURL PHP extension.');
+}
+if (!function_exists('json_decode')){
+    die ('MercadoPago needs the JSON PHP extension.');
+}
+
+class MercadoPago {
+
+    /*
+     * Version
+     */
+    private $version = "3.0.1";
+
+    /*
+     * Parametros
+     */
+    private $config = array();
+    private $client = array();
+    private $item = array();
+    private $all = array();
+
+    public function  __construct($args = array()) {
+        $this->setConfig($args);
+    }
+
+    public function getVersion(){
+        return $this->version;
+    }
+
+    public function getAllParams(){
+        $this->all = $this->config + $this->item + $this->client;
+    }
+
+    public function showButton(){
+
+        $this->getAllParams();
+
+        $html  = '<form target="_top" action="https://www.mercadopago.com/mlb/buybutton" method="post">';
+        $html .= '  <input type="image" src="https://www.mercadopago.com/org-img/MP3/buy_now_07_mlb.gif" border="0" alt="Comprar Agora">';
+
+        foreach ($this->all as $field => $value) {
+            $html .= "  <input type=\"hidden\" name=\"$field\" value=\"$value\">\n";
+        }
+
+        $html .= '</form>';
+
+        return $html;
+    }
+
+    public function setClient($args = array()){
+        $this->client = $args;
+    }
+
+    public function setItem($args = array()){
+        $this->item = $args;
+    }
+
+    private function setConfig( $args = array() ){
+        $this->config = $args;
+    }
+
+}
+?>
--- src/examples/button.php
+++ src/examples/button.php
@@ -1,8 +1,39 @@
-﻿<?php
+<?php
 
-require_once("../src/mercadopago/mpago.php");
+include("../classes/MercadoPago.php");
 
+$mp = new MercadoPago(array(
+    'acc_id' => '19767148',
+    'enc' => 'Z7VHQVdZYgyaqKbGGKxhYGLSMho%3D',
+    'url_succesfull' => 'http://www.mercadopago.com/sucesso',
+    'url_process' => 'http://www.mercadopago.com/analise',
+    'url_cancel' => 'http://www.mercadopago.com/cancel',
+    'currency' => 'REA',
+    'ship_cost_mode' => 'DS',
+    'shipping_cost' => ''
+));
 
+$mp->setClient(array(
+    'cart_name' => 'Carlos',
+    'cart_surname' => 'Corrêa',
+    'cart_email' => 'carlos.correa@mercadopago.com.br',
+    'cart_cep' => '06541-005',
+    'cart_street' => 'Avenida Marte',
+    'cart_number' => '489',
+    'cart_complement' => '',
+    'cart_phone' => '11-25435400',
+    'cart_district' => 'Alphaville',
+    'cart_city' => 'Santana de Parnaíba',
+    'cart_state' => 'SP',
+));
+
+$mp->setItem(array(
+    'item_id' => '789456',
+    'name' => 'Nome_do_Produto',
+    'price' => '12.67'
+));
+
+echo $mp->showButton();
 
 
 ?>
