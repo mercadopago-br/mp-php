b749d4be9e405a7dd2aff84d2e0475373e67a01f
Carlos Correa <carlos.correa@mercadopago.com.br>
2011-03-30 16:18:39 -0300

    remover dados

--- src/examples/button.php
+++ src/examples/button.php
@@ -20,25 +20,25 @@
 include("../classes/MercadoPago.php");
 
 $mp = new MercadoPago(array(
-    'acc_id' => '19767148',
-    'enc' => 'Z7VHQVdZYgyaqKbGGKxhYGLSMho%3D',
-    'url_succesfull' => 'http://www.mercadopago.com/sucesso',
-    'url_process' => 'http://www.mercadopago.com/analise',
-    'url_cancel' => 'http://www.mercadopago.com/cancel',
+    'acc_id' => 'XXXXXX',
+    'enc' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'url_succesfull' => 'http://www.seusite.com/sucesso',
+    'url_process' => 'http://www.seusite.com/analise',
+    'url_cancel' => 'http://www.seusite.com/cancel',
     'currency' => 'REA',
     'ship_cost_mode' => 'DS',
     'shipping_cost' => ''
 ));
 
 $mp->setClient(array(
-    'cart_name' => 'Carlos',
-    'cart_surname' => 'Corrêa',
-    'cart_email' => 'carlos.correa@mercadopago.com.br',
-    'cart_cep' => '06541-005',
+    'cart_name' => 'Nome',
+    'cart_surname' => 'Sobrenome',
+    'cart_email' => 'email_do_cliente@seusite.com.br',
+    'cart_cep' => 'XXXXX-XXX',
     'cart_street' => 'Avenida Marte',
     'cart_number' => '489',
     'cart_complement' => '',
-    'cart_phone' => '11-25435400',
+    'cart_phone' => '11-XXXXXXXX',
     'cart_district' => 'Alphaville',
     'cart_city' => 'Santana de Parnaíba',
     'cart_state' => 'SP',
